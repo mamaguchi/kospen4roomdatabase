8a1b2d610d84063e749f51ac1c3499745b7117d7
patrick <patrickchow688@gmail.com>
2018-03-01 18:47:01 +0800

    1March2018 - Android able to update 'version' column of local 'kospenuser'(that generates both OutRestReqKospenuser & InDBQueryKospenuser) on every REST request of inside-locality kospenusers list from laravel server.

--- app/src/main/java/com/example/intel/kospenmove02/db/dao/KospenuserDao.java
+++ app/src/main/java/com/example/intel/kospenmove02/db/dao/KospenuserDao.java
@@ -17,6 +17,7 @@ import static android.arch.persistence.room.OnConflictStrategy.IGNORE;
 @Dao
 public interface KospenuserDao {
 
+    // VERSION 1: using LiveData -> good for testing sql statements to see if desired data is return by Android sqlite
 //    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
 //            "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
 //            "FROM kospenuser INNER JOIN kospenuserserver " +
@@ -70,28 +71,83 @@ public interface KospenuserDao {
 //    LiveData<List<Kospenuser>> loadScenarioFive();
 
 
-    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+    // VERSION 2: using normal java list -> when LiveData is no longer required to update UI
+//    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+//            "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
+//            "FROM kospenuser INNER JOIN kospenuserserver " +
+//            "ON kospenuserserver.ic = kospenuser.ic " +
+//            "WHERE kospenuser.timestamp > kospenuserserver.timestamp")
+//    List<Kospenuser> loadScenarioOne();
+//
+//    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+//            "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
+//            "FROM kospenuser INNER JOIN kospenuserserver " +
+//            "ON kospenuserserver.ic = kospenuser.ic " +
+//            "WHERE kospenuser.timestamp < kospenuserserver.timestamp")
+//    List<Kospenuser> loadScenarioTwo();
+//
+//    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+//            "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
+//            "FROM kospenuser INNER JOIN kospenuserserver " +
+//            "ON kospenuserserver.ic = kospenuser.ic " +
+//            "WHERE kospenuser.timestamp = kospenuserserver.timestamp")
+//    List<Kospenuser> loadScenarioThree();
+//
+//    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+//            "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
+//            "FROM kospenuser JOIN kospenuserglobal " +
+//            "ON kospenuser.ic = kospenuserglobal.ic " +
+//            "WHERE kospenuser.ic NOT in (SELECT ic FROM kospenuserserver) " +
+//            "AND (kospenuser.timestamp > kospenuserglobal.timestamp)")
+//    List<Kospenuser> loadScenarioFourA();
+//
+//    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+//            "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
+//            "FROM kospenuser JOIN kospenuserglobal " +
+//            "ON kospenuser.ic = kospenuserglobal.ic " +
+//            "WHERE kospenuser.ic NOT in (SELECT ic FROM kospenuserserver) " +
+//            "AND (kospenuser.timestamp < kospenuserglobal.timestamp)")
+//    List<Kospenuser> loadScenarioFourB();
+//
+//    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+//            "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
+//            "FROM kospenuser JOIN kospenuserglobal " +
+//            "ON kospenuser.ic = kospenuserglobal.ic " +
+//            "WHERE kospenuser.ic NOT in (SELECT ic FROM kospenuserserver) " +
+//            "AND (kospenuser.timestamp = kospenuserglobal.timestamp)")
+//    List<Kospenuser> loadScenarioFourC();
+//
+//    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+//            "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
+//            "FROM kospenuser " +
+//            "WHERE ic NOT in (SELECT ic FROM kospenuserserver) " +
+//            "AND ic NOT in (SELECT ic FROM kospenuserglobal)")
+//    List<Kospenuser> loadScenarioFive();
+
+
+    // VERSION 3: to update 'version' column of local 'kospenuser' on every REST request of inside-locality kospenusers list from laravel server
+    @Query("SELECT kospenuserserver.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
             "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
             "FROM kospenuser INNER JOIN kospenuserserver " +
             "ON kospenuserserver.ic = kospenuser.ic " +
             "WHERE kospenuser.timestamp > kospenuserserver.timestamp")
     List<Kospenuser> loadScenarioOne();
 
-    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+    @Query("SELECT kospenuserserver.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
             "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
             "FROM kospenuser INNER JOIN kospenuserserver " +
             "ON kospenuserserver.ic = kospenuser.ic " +
             "WHERE kospenuser.timestamp < kospenuserserver.timestamp")
     List<Kospenuser> loadScenarioTwo();
 
-    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+    @Query("SELECT kospenuserserver.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
             "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
             "FROM kospenuser INNER JOIN kospenuserserver " +
             "ON kospenuserserver.ic = kospenuser.ic " +
             "WHERE kospenuser.timestamp = kospenuserserver.timestamp")
     List<Kospenuser> loadScenarioThree();
 
-    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+    @Query("SELECT kospenuserglobal.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
             "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
             "FROM kospenuser JOIN kospenuserglobal " +
             "ON kospenuser.ic = kospenuserglobal.ic " +
@@ -99,7 +155,7 @@ public interface KospenuserDao {
             "AND (kospenuser.timestamp > kospenuserglobal.timestamp)")
     List<Kospenuser> loadScenarioFourA();
 
-    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+    @Query("SELECT kospenuserglobal.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
             "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
             "FROM kospenuser JOIN kospenuserglobal " +
             "ON kospenuser.ic = kospenuserglobal.ic " +
@@ -107,7 +163,7 @@ public interface KospenuserDao {
             "AND (kospenuser.timestamp < kospenuserglobal.timestamp)")
     List<Kospenuser> loadScenarioFourB();
 
-    @Query("SELECT kospenuser.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
+    @Query("SELECT kospenuserglobal.version, kospenuser.timestamp, kospenuser.ic, kospenuser.name, kospenuser.fk_gender, kospenuser.address, " +
             "kospenuser.fk_state, kospenuser.fk_region, kospenuser.fk_subregion, kospenuser.fk_locality, kospenuser.firstRegRegion " +
             "FROM kospenuser JOIN kospenuserglobal " +
             "ON kospenuser.ic = kospenuserglobal.ic " +
@@ -121,6 +177,7 @@ public interface KospenuserDao {
             "WHERE ic NOT in (SELECT ic FROM kospenuserserver) " +
             "AND ic NOT in (SELECT ic FROM kospenuserglobal)")
     List<Kospenuser> loadScenarioFive();
+
 
     @Query("SELECT * from kospenuser")
     LiveData<List<Kospenuser>> loadAllKospenusers();
--- app/src/main/java/com/example/intel/kospenmove02/db/dao/KospenuserServerDao.java
+++ app/src/main/java/com/example/intel/kospenmove02/db/dao/KospenuserServerDao.java
@@ -16,12 +16,15 @@ import static android.arch.persistence.room.OnConflictStrategy.IGNORE;
 @Dao
 public interface KospenuserServerDao {
 
+    // VERSION 1: using LiveData -> good for testing sql statements to see if desired data is return by Android sqlite
 //    @Query("SELECT kospenuserserver.version, kospenuserserver.timestamp, kospenuserserver.ic, kospenuserserver.name, kospenuserserver.fk_gender, kospenuserserver.address, " +
 //            "kospenuserserver.fk_state, kospenuserserver.fk_region, kospenuserserver.fk_subregion, kospenuserserver.fk_locality, kospenuserserver.firstRegRegion " +
 //            "FROM kospenuserserver " +
 //            "WHERE ic NOT in (SELECT ic FROM kospenuser)")
 //    LiveData<List<KospenuserServer>> loadScenarioSix();
 
+
+    // VERSION 2: using normal java list -> when LiveData is no longer required to update UI
     @Query("SELECT kospenuserserver.version, kospenuserserver.timestamp, kospenuserserver.ic, kospenuserserver.name, kospenuserserver.fk_gender, kospenuserserver.address, " +
             "kospenuserserver.fk_state, kospenuserserver.fk_region, kospenuserserver.fk_subregion, kospenuserserver.fk_locality, kospenuserserver.firstRegRegion " +
             "FROM kospenuserserver " +
